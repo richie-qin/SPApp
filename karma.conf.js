c514c5fbca0e85ce45c983df970e0792768ef789
richie.qin <qinfuguan@richie.local>
2019-06-25 09:59:24 +0800

    创建新项目

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/SPAPP'),
+      dir: require('path').join(__dirname, './coverage/pinduoduo'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
