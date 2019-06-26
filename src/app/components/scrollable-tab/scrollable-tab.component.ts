b35f6e7709590b0ab4f70e2de911c3279350206f
richie.qin <qinfuguan@richie.local>
2019-06-26 09:05:15 +0800

    新建组件

---
+++ src/app/components/scrollable-tab/scrollable-tab.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-scrollable-tab',
+  templateUrl: './scrollable-tab.component.html',
+  styleUrls: ['./scrollable-tab.component.less']
+})
+export class ScrollableTabComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
